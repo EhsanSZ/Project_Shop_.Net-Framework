00896b2f3bc761317687fea60dc154ef69932912
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-07-13 01:43:54 +0430

    Feature Parent

---
+++ EShop/Areas/Admin/Controllers/FeaturesController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DataLayer;
+
+namespace EShop.Areas.Admin.Controllers
+{
+    public class FeaturesController : Controller
+    {
+        private MyEShopEntities db = new MyEShopEntities();
+
+        public ActionResult Index()
+        {
+            return View(db.Features.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FeatureID,FeatureTitle")] Features features)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Features.Add(features);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(features);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Features features = db.Features.Find(id);
+            if (features == null)
+            {
+                return HttpNotFound();
+            }
+            return View(features);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "FeatureID,FeatureTitle")] Features features)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(features).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(features);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Features features = db.Features.Find(id);
+            if (features == null)
+            {
+                return HttpNotFound();
+            }
+            return View(features);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Features features = db.Features.Find(id);
+            db.Features.Remove(features);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
